39807195ed730ca2875f82839b1617fbced60232
nikodyulger <nikolasvdulgerov@gmail.com>
2024-06-28 22:59:55 +0200

    sagemaker execution role

--- notebooks/infraestructura/__main__.py
+++ notebooks/infraestructura/__main__.py
@@ -1,10 +1,30 @@
+import json
 import pulumi
-from pulumi_aws import s3
+import pulumi_aws as aws
 
-bucket = s3.Bucket(
+bucket = aws.s3.Bucket(
     "recipes_data_models_sagemaker_bucket",
     bucket="recipes-data-models-sagemaker-bucket",
 )
 
+assume_role_policy = aws.iam.get_policy_document(
+    statements=[
+        {
+            "actions": ["sts:AssumeRole"],
+            "principals": [
+                {
+                    "identifiers": ["sagemaker.amazonaws.com"],
+                    "type": "Service",
+                }
+            ],
+        }
+    ]
+)
+
+sagemaker_execution_role = aws.iam.Role(
+    "sagemaker_execution_role",
+    assume_role_policy=assume_role_policy.json,
+    managed_policy_arns=[aws.iam.ManagedPolicy.AMAZON_SAGE_MAKER_FULL_ACCESS],
+)
 
 pulumi.export("bucket_name", bucket.id)
